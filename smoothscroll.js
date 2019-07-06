47d21f029310825b358c9fc653cdeb683311d443
ljl22 <52179264+ljl22@users.noreply.github.com>
2019-07-06 12:10:48 -0400

    Add files via upload
    Added javascript for smoothscroll

---
+++ smoothscroll.js
@@ -0,0 +1,14 @@
+$(function() { 
+ 
+  // scrollSpeed(step, speed);
+
+  jQuery.scrollSpeed(100, 800);
+    
+});
+
+$(function() {  
+
+  // scrollSpeed(step, speed, easing);
+  jQuery.scrollSpeed(100, 800, 'easeInOutBack');
+    
+});
